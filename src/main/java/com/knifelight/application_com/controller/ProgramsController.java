e87fec822a33a2f7eaf517e934b2017a316d66e5
Li-Zongyao <lizongyao97@163.com>
2019-05-05 23:41:57 +0800

    display program finish

--- src/main/java/com/knifelight/application_com/controller/ProgramsController.java
+++ src/main/java/com/knifelight/application_com/controller/ProgramsController.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RestController
 public class ProgramsController {
@@ -29,11 +33,27 @@ public class ProgramsController {
         temPrograms.setSubject(program.getSubject());
         temPrograms.setApptime(program.getApptime());
         temPrograms.setStatus("1");
-        System.out.println("   getApplicantunit:"+ program.getApplicantunit());
-        System.out.println("temProgramsId : "+temPrograms.getId());
+        System.out.printf("   getApplicantunit:"+ program.getApplicantunit());
+        System.out.printf("temProgramsId : "+temPrograms.getId());
         programsrepository.save(temPrograms);
+        returnmessage = "save finish";
 
         return returnmessage;
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/api/programlist", method = RequestMethod.POST, produces = "application/json; charset=UTF-8")
+    @ResponseBody
+    public Map<String,Object> getprogram(@RequestBody Programs program) {
+        Map<String,Object> returnMap=new HashMap<String,Object>();
+        System.out.println("开始查询在线视频资源");
+        List<Programs> list = null;
+        list = programsrepository.findAll();
+        System.out.println("结束查询在线视频资源" + list );
+        String str2 = new String("return programs success");
+        returnMap.put("aaa", str2 );
+        returnMap.put("program", list);
+        return returnMap;
     }
 
 
